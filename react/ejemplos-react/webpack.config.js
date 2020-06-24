4a9b323fcdcdfb99fc47d75dbf187afc71b3b0a7
AngelVil92 <angelvil92@gmail.com>
2020-06-24 15:49:23 +0200

    Ejemplo 1

---
+++ react/ejemplos-react/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const entryPath = path.join(__dirname, 'src');
+const outputPath = path.join(__dirname, 'dist');
+
+module.exports = {
+  entry: path.join(entryPath, 'app.js'),
+  output: {
+    path: outputPath,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  devServer: {
+    contentBase: outputPath
+  }
+}
